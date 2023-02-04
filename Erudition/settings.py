1b0c96301995a5d9d7f680033f0b7158389625b5
TA-800 <taherali0905@gmail.com>
2023-02-04 17:49:30 -0500

    imgs fix attempt 1

--- Erudition/settings.py
+++ Erudition/settings.py
@@ -179,6 +179,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type

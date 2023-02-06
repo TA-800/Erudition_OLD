4ed442f7c675fdef551ff2099b6f75a93e9a4c78
TA-800 <taherali0905@gmail.com>
2023-02-06 14:50:25 -0500

    deploy fix test

--- Erudition/settings.py
+++ Erudition/settings.py
@@ -22,7 +22,7 @@ from datetime import timedelta
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-load_dotenv()
+# load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.config('SECRET_KEY')

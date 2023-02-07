4d709149fd9a32c0f60728c6cadfe3a5d5977d0f
TA-800 <taherali0905@gmail.com>
2023-02-07 17:52:47 -0500

    updated landing

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,46 +1,77 @@
+import {
+    faClock,
+    faComment,
+    faCommentDots,
+    faICursor,
+    faMousePointer,
+    faPerson,
+    faSign,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
     return (
-        <div className="flex flex-col gap-32">
+        <div className="grid grid-cols-2 sm:grid-cols-1">
             {/* Nice looking landing page keeping in theme of the website */}
-            <Division
-                title="Looking for an easy, hassle-free way to organize notes and tasks?"
-                information="Well, you've come to the right place! Erudition is a platform for students where they can put together all
+            <div className="col-span-2 sm:col-span-1 mb-5">
+                <Division
+                    title="Looking for an easy, hassle-free way to organize notes and tasks?"
+                    information="Well, you've come to the right place! Erudition is a platform for students where they can put together all
                     important study material and information in one neatly-organized place. It is a place where students can
                     discuss their doubts and help each other out. Also a place to relax!"
-                isTitle
+                    isTitle
+                />
+            </div>
+            <Division
+                title="Easy to use"
+                information="Easily navigate and quickly create stylized notes or assignments."
+                icon={faClock}
             />
-
-            <Division title="Easy to use" information="Easy to use, navigate, and create stylized notes or assignments." />
             <Division
                 title="Discuss with friends"
                 information="Know how others are doing by chatting with them in the discussions page."
+                icon={faCommentDots}
             />
             <Division
                 title="Personalize profiles"
                 information="Add a picture to your profile and other information about yourself."
+                icon={faPerson}
             />
-            <Division title="Get started!" information="Get started by creating an account and logging in." isLogin />
+            <Division
+                title="Get started!"
+                information="Get started by creating an account and logging in."
+                isLogin
+                icon={faMousePointer}
+            />
         </div>
     );
 }
 
-function Division({ title, information, isTitle, isLogin }) {
+function Division({ title, information, isTitle, isLogin, icon }) {
     const navigate = useNavigate();
 
     return (
-        <div className={"flex flex-col gap-2 " + (isTitle ? "" : "items-center")}>
+        <div
+            className={
+                "relative flex flex-col justify-center gap-2 p-2 select-none " +
+                (isTitle
+                    ? ""
+                    : "h-96 items-center border-2 border-zinc-500 border-opacity-50 hover:bg-zinc-500 hover:bg-opacity-5")
+            }>
             {/* If orientation is right, make the title right-ended. If left, do nothing (or stick to defaults) */}
-            <p className={isTitle ? "text-7xl mdc:text-5xl font-black mt-20 mdc:mt-5" : "text-5xl mdc:text-3xl font-bold"}>
-                {title}
-            </p>
-            <p className="info-text">{information}</p>
+            <p className={isTitle ? "text-7xl mdc:text-5xl font-black" : "text-5xl mdc:text-3xl font-bold"}>{title}</p>
+            <p className="info-text max-w-none">{information}</p>
             {isLogin && (
-                <button className="btn-light" onClick={() => navigate("/login")}>
+                <button className="btn-light z-10" onClick={() => navigate("/login")}>
                     Click here to sign up.
                 </button>
+            )}
+            {!isTitle && (
+                <div className="absolute text-3xl md:text-2xl sm:text-xl opacity-10 z-0">
+                    <FontAwesomeIcon icon={icon} size="10x" />
+                </div>
             )}
         </div>
     );
